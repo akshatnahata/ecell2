9d0fd57dcbcc2917f0efadab653d3b34b8599a3c
Krish Agrawal <diwankrish.17@gmail.com>
2022-03-31 23:24:48 +0530

    Testing Netlify connection

--- ecell_new/src/App.js
+++ ecell_new/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This will be E-Cell IITI New Website
         </p>
         <a
           className="App-link"
